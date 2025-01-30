f8acade518188c6c4e9c97f153e461aba718106b
Williams Medina <90166309+williamsmedina@users.noreply.github.com>
2025-01-30 09:45:30 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.20.0"  # Asegúrate de usar una versión que soporte Availability Zones
+      version = ">= 3.20.0" 
     }
   }
   required_version = ">= 1.1.0"
